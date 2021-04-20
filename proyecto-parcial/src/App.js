e5019ffd12ba8cc82bc09f813e7cace53035bfc4
Franco Mendelsohn <56727209+franco-mendelsohn@users.noreply.github.com>
2021-04-20 17:55:13 -0300

    maquetado

--- proyecto-parcial/src/App.js
+++ proyecto-parcial/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import database from "./database.json"
+import Tarjeta from './components/Tarjeta'
+import Header from './components/Header'
+
+
+function App (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className="todo">
+
+
+      <Header/>
+
+
+      <div className="caja">
+          {database.map(function(unPersonaje, idx){
+            return (<Tarjeta 
+                    personaje = {unPersonaje} 
+                    key={idx}
+                     />)
+            }
+          )}
+      </div> 
+
+      <footer>
+        <h1>Created by: Franco Mendelsohn</h1>
+      </footer>
+
+
+    </div>     
   );
+
 }
 
 export default App;
+
---
+++ proyecto-parcial/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header () {
+		return(
+			<header>
+         <div> 
+				 <h1> <span class="colorAzul">  </span> </h1>
+		 </div>
+      </header>
+		);
+	}
+
+	export default Header;
+
+
+
