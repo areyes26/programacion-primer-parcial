9ae4ecbebd7f066a21abc8c74495a242ff830e48
areyes26 <areyesabarca@udesa.edu.ar>
2021-04-21 11:32:25 -0300

    botones

--- proyecto-parcial/src/components/Tarjeta.js
+++ proyecto-parcial/src/components/Tarjeta.js
@@ -1,12 +1,16 @@
 function Tarjeta (props) {
 		return(
 			<div className="tarjeta">
+				<a href="http://google.com">
 				<img src={props.personaje.picture.large} alt=""/>
 				<h2> {props.personaje.name.first} {props.personaje.name.last} </h2>
 				 
 				<h3>{props.personaje.dob.date} ({props.personaje.dob.age} años)</h3> 
 				<h3>{props.personaje.email}</h3>
-				
+				<button>Agregar</button>
+				<br/>
+				<button>Eliminar</button>
+				</a>
 				
 			</div>
 		);
