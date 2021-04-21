a09c428065d1e579117719b295858c555a4baf30
areyes26 <areyesabarca@udesa.edu.ar>
2021-04-21 11:24:00 -0300

    class App

--- proyecto-parcial/src/App.js
+++ proyecto-parcial/src/App.js
@@ -1,39 +1,49 @@
 
-
+import { Component } from 'react';
 import database from "./database.json"
 import Tarjeta from './components/Tarjeta'
 import Header from './components/Header'
 
 
-function App (){
-  return (
+class App extends Component {
+
+  constructor(){
+		super();
+		this.state={
+      
+		}
+	};
+
+  render (){
+    return (
 
 
 
-    <div className="todo">
-
-
-      <Header/>
-
-
-      <div className="caja">
-          {database.map(function(unPersonaje, idx){
-            return (<Tarjeta 
-                    personaje = {unPersonaje} 
-                    key={idx}
-                     />)
-            }
-          )}
-      </div> 
-
-      <footer>
-        <h1>Created by: Franco Mendelsohn</h1>
-      </footer>
-
-
-    </div>     
-
-  );
+      <div className="todo">
+  
+  
+        <Header/>
+  
+  
+        <div className="caja">
+            {database.map(function(unPersonaje, idx){
+              return (<Tarjeta 
+                      personaje = {unPersonaje} 
+                      key={idx}
+                       />)
+              }
+            )}
+        </div> 
+  
+        <footer>
+          <h1>Created by: Franco Mendelsohn</h1>
+        </footer>
+  
+  
+      </div>     
+  
+    );
+  }
 
 }
 
